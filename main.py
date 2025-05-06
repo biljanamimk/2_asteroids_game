9e84a4e22bcd8c70bf83208c415b28757bd39eec
biljanamimk <37456805+biljanamimk@users.noreply.github.com>
2025-05-06 15:02:27 +0200

    Environment set up, main.py and constants.py added

---
+++ main.py
@@ -0,0 +1,14 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+from constants import *
+import pygame
+
+def main():
+  """This is the main function that prints a greeting."""
+  print("Starting Asteroids!")
+  print(f"Screen width: {SCREEN_WIDTH}")
+  print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+  main()
